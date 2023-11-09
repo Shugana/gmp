a707551190c5334a85a17ce1e64f2b8c4a1bc432
Rachel <shugana@gmail.com>
2023-11-10 00:06:31 +0100

    first commit

---
+++ gamemodes/new.lua
@@ -0,0 +1,33 @@
+
+function OnGamemodeInit()
+  SetServerDescription("Fluffy Server");
+  SetGamemodeName("Fluff Local Test");
+  EnableNickname(0);
+  EnableExitGame(0);
+  EnableChat(0);
+  math.randomseed(os.time());
+end
+
+function OnPlayerConnect(playerid)
+  SendPlayerMessage(playerid, 207,175,55, "connected to the fluff server, woop woop");
+  SetPlayerWorld(playerid, "NEWWORLD\\KHORINIS.ZEN");
+  SetPlayerPos(playerid, 0, 0, 0);
+end
+
+function OnPlayerText(playerid, text)
+  if not(text) then
+    return 0;
+  end
+  SendPlayerMessage(playerid, 207,175,55, "text send was: "..text);
+  return 0;
+end
+
+function OnPlayerCommandText(playerid, command)
+  if not(command) then
+    return 0;
+  end
+  SendPlayerMessage(playerid, 207,175,55, "command send was: "..command);
+  if command == "/q" then
+	  ExitGame(playerid);
+  end
+end
