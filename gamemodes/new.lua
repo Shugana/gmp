bd43da848dff28dd88817170975658caaffe3968
Rachel <shugana@gmail.com>
2023-11-10 02:14:57 +0100

    update

--- gamemodes/new.lua
+++ gamemodes/new.lua
@@ -85,7 +85,7 @@ function OnServerWorldTime(oldHour, oldMinute, newHour, newMinute)
   end
   for spawnid, spawndata in pairs(SPAWNS) do
     if (spawndata.spawned == false) then
-      local itemid = CreateItem(row.iteminstance, 1, spawndata.location.x, spawndata.location.y, spawndata.location.z);
+      local itemid = CreateItem(spawndata.item, 1, spawndata.location.x, spawndata.location.y, spawndata.location.z, "NEWWORLD\\KHORINIS.ZEN");
       WORLDITEMS[itemid] = spawnid;
       SPAWNS[spawnid].spawned = true;
     end
