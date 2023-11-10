b90d1a5fa284b1b81364fad6ebafe8d8a0d6b9d3
Rachel <shugana@gmail.com>
2023-11-10 17:25:01 +0100

    Untrack files in .gitignore

--- settings/db.lua
+++
@@ -1,9 +0,0 @@
-DB = {
-    HANDLER = nil,
-    IP = "localhost",
-    USER = "dbuser",
-    PASSWORD = "dbpasswd",
-    DBNAME = "dbname",
-    PORT = 3306,
-    SOCKET = "/run/mysqld/mysqld.sock"
-};
