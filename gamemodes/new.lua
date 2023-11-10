d5b68cc77661a556248eb190ce83422b781ee1f3
Rachel <shugana@gmail.com>
2023-11-10 01:52:21 +0100

    update

--- gamemodes/new.lua
+++ gamemodes/new.lua
@@ -10,6 +10,7 @@ end
 function OnPlayerConnect(playerid)
   SetPlayerWorld(playerid, "NEWWORLD\\KHORINIS.ZEN");
   SetPlayerPos(playerid, 0, 0, 0);
+  SpawnPlayer(playerid);
 end
 
 function OnPlayerText(playerid, text)
