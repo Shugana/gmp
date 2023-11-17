f40da43afc72692b5dd33a7d32ab70ec460e1b62
Rachel <shugana@gmail.com>
2023-11-17 20:34:00 +0100

    logging and regular chat

--- scripts/logging.lua
+++ scripts/logging.lua
@@ -1,6 +1,6 @@
 function log(msgtype, msg)
 	local cur_date = os.date("%Y_%m_%d")
-	if not(exists("logs/"..msgtype.."/")) then
+	if not(os.rename("logs/"..msgtype.."/","logs/"..msgtype.."/") and true or false) then
 		os.execute("mkdir -pv logs/"..msgtype);
 	end
 	LogString("logs/" .. string.lower(msgtype) .."/"..string.lower(msgtype).. "_" .. cur_date, msg);
