7e9c01ada025044663d774518c8e5ceb1ac9c37c
Rachel <shugana@gmail.com>
2023-11-19 17:48:05 +0100

    focus now tracks your target and gives information

--- scripts/focus.lua
+++ scripts/focus.lua
@@ -1,27 +1,21 @@
 function OnPlayerFocus(playerid, focusid)
-    debug("focuschange on "..playerid.." to focus "..focusid);
     local text = nil;
     if (PLAYERS[playerid].focus ~= nil) then
         DestroyPlayerDraw(playerid, PLAYERS[playerid].focus);
         PLAYERS[playerid].focus = nil;
-        debug("old focus text removed");
     end
 
     if PLAYERS[focusid] == nil and NPCS[focusid] == nil then
-        debug("focus not a player, not a monster, maybe nothing in focus");
         return;
     end
 
-    text = GetPlayerName(focusid).." ("..focusid..")";
-    debug(text);
+    text = "Fokus: "..GetPlayerName(focusid).." ("..focusid..")";
     
     if WORLDMONSTERS[focusid] ~= nil then
         text = text.." ["..WORLDMONSTERS[focusid].."]";
-        debug(text);
     end
-    x, y = convertToPixel(30, 1010);
-    PLAYERS[playerid].focus = CreatePlayerDraw(playerid, x, y, text, FONTS.standard, 255, 255, 255);
+    x, y = convertToPixel(30, 923);
+    PLAYERS[playerid].focus = CreatePlayerDraw(playerid, x, y, text, FONTS.sequel, 255, 255, 255);
     ShowPlayerDraw(playerid, PLAYERS[playerid].focus);
-    debug("focus function complete");
 end
 
