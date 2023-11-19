9d7a047cd1db31e25f5d8d1338ef327168b847c3
Rachel <shugana@gmail.com>
2023-11-19 09:08:38 +0100

    tp now works

--- scripts/utility.lua
+++ scripts/utility.lua
@@ -77,7 +77,9 @@ function showAni(playerid, params)
 end
 
 function getPlayerIdByName(playerid, name)
+    debug("Getplayeridbyname-search: "..name);
     for targetid, _data in pairs(PLAYERS) do
+        debug("Getplayeridbyname-target: "..GetPlayerName(targetid));
         if (GetPlayerName(targetid) == name) then 
             return targetid;
         end
