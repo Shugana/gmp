ebccd1dad2024a7f98f62788bbad410b3de4ad83
Rachel <shugana@gmail.com>
2023-11-23 00:22:06 +0100

    teach simulator

--- scripts/teach.lua
+++ scripts/teach.lua
@@ -27,28 +27,27 @@ function teachmenu(playerid)
     local size = {x=450, y=50};
     local start = {x=500, y=350};
     local teachcosts = getNextTeachCost(playerid);
-    local lp = GetPlayerLearnPoints(playerid);
-    if (lp >= teachcosts.cost) then
-        createButton(playerid, "Stärke "..teachcosts.str.." -> "..(teachcosts.str+1).." ("..teachcosts.cost..")", start.x, start.y+0*size.y, size.x, size.y, 0, 255, 152, "teach", {stat = "str", cost = teachcosts.cost, lp=lp, oldstat=teachcosts.str});
-        createButton(playerid, "Geschick "..teachcosts.dex.." -> "..(teachcosts.dex+1).." ("..teachcosts.cost..")", start.x, start.y+1*size.y, size.x, size.y, 0, 255, 152, "teach", {stat = "dex", cost = teachcosts.cost, lp=lp, oldstat=teachcosts.dex});
-        createButton(playerid, "Mana "..teachcosts.maxmana.." -> "..(teachcosts.maxmana+1).." ("..teachcosts.maxmana..")", start.x, start.y+2*size.y, size.x, size.y, 0, 255, 152, "teach", {stat = "maxmana", cost = teachcosts.cost, lp=lp, oldstat=teachcosts.maxmana});
+    if (teachcosts.lp >= teachcosts.cost) then
+        createButton(playerid, "Stärke "..teachcosts.str.." -> "..(teachcosts.str+1).." ("..teachcosts.cost..")", start.x, start.y+0*size.y, size.x, size.y, 0, 255, 152, "teach", {stat = "str", cost = teachcosts.cost, lp=teachcosts.lp, oldstat=teachcosts.str});
+        createButton(playerid, "Geschick "..teachcosts.dex.." -> "..(teachcosts.dex+1).." ("..teachcosts.cost..")", start.x, start.y+1*size.y, size.x, size.y, 0, 255, 152, "teach", {stat = "dex", cost = teachcosts.cost, lp=teachcosts.lp, oldstat=teachcosts.dex});
+        createButton(playerid, "Mana "..teachcosts.maxmana.." -> "..(teachcosts.maxmana+1).." ("..teachcosts.maxmana..")", start.x, start.y+2*size.y, size.x, size.y, 0, 255, 152, "teach", {stat = "maxmana", cost = teachcosts.cost, lp=teachcosts.lp, oldstat=teachcosts.maxmana});
         if (teachcosts.onehanded < 100) then
-            createButton(playerid, "Einhand "..teachcosts.onehanded.." -> "..(teachcosts.onehanded+1).." ("..teachcosts.cost..")", start.x, start.y+3*size.y, size.x, size.y, 0, 255, 152, "teach", {stat = "onehanded", cost = teachcosts.cost, lp=lp, oldstat=teachcosts.onehanded});
+            createButton(playerid, "Einhand "..teachcosts.onehanded.." -> "..(teachcosts.onehanded+1).." ("..teachcosts.cost..")", start.x, start.y+3*size.y, size.x, size.y, 0, 255, 152, "teach", {stat = "onehanded", cost = teachcosts.cost, lp=teachcosts.lp, oldstat=teachcosts.onehanded});
         else
             createText(playerid, "Einhand - maximum - 100", start.x, start.y+3*size.y, size.x, size.y, 196, 30, 58);
         end
         if (teachcosts.twohanded < 100) then
-            createButton(playerid, "Zweihand "..teachcosts.twohanded.." -> "..(teachcosts.twohanded+1).." ("..teachcosts.cost..")", start.x, start.y+4*size.y, size.x, size.y, 0, 255, 152, "teach", {stat = "twohanded", cost = teachcosts.cost, lp=lp, oldstat=teachcosts.twohanded});
+            createButton(playerid, "Zweihand "..teachcosts.twohanded.." -> "..(teachcosts.twohanded+1).." ("..teachcosts.cost..")", start.x, start.y+4*size.y, size.x, size.y, 0, 255, 152, "teach", {stat = "twohanded", cost = teachcosts.cost, lp=teachcosts.lp, oldstat=teachcosts.twohanded});
         else
             createText(playerid, "Zweihand - maximum - 100", start.x, start.y+4*size.y, size.x, size.y, 196, 30, 58);
         end
         if (teachcosts.bow < 100) then
-            createButton(playerid, "Bogen "..teachcosts.bow.." -> "..(teachcosts.bow+1).." ("..teachcosts.cost..")", start.x, start.y+5*size.y, size.x, size.y, 0, 255, 152, "teach", {stat = "bow", cost = teachcosts.cost, lp=lp, oldstat=teachcosts.bow});
+            createButton(playerid, "Bogen "..teachcosts.bow.." -> "..(teachcosts.bow+1).." ("..teachcosts.cost..")", start.x, start.y+5*size.y, size.x, size.y, 0, 255, 152, "teach", {stat = "bow", cost = teachcosts.cost, lp=teachcosts.lp, oldstat=teachcosts.bow});
         else
             createText(playerid, "Bogen - maximum - 100", start.x, start.y+5*size.y, size.x, size.y, 196, 30, 58);
         end
         if (teachcosts.crossbow < 100) then
-            createButton(playerid, "Armbrust "..teachcosts.crossbow.." -> "..(teachcosts.crossbow+1).." ("..teachcosts.cost..")", start.x, start.y+6*size.y, size.x, size.y, 0, 255, 152, "teach", {stat = "crossbow", cost = teachcosts.cost, lp=lp, oldstat=teachcosts.crossbow});
+            createButton(playerid, "Armbrust "..teachcosts.crossbow.." -> "..(teachcosts.crossbow+1).." ("..teachcosts.cost..")", start.x, start.y+6*size.y, size.x, size.y, 0, 255, 152, "teach", {stat = "crossbow", cost = teachcosts.cost, lp=teachcosts.lp, oldstat=teachcosts.crossbow});
         else
             createText(playerid, "Armbrust - maximum - 100", start.x, start.y+6*size.y, size.x, size.y, 196, 30, 58);
         end
@@ -70,7 +69,7 @@ function teach(playerid, args)
 end
 
 function getNextTeachCost(playerid)
-    local responses = DB_select("id, (maxmana+str+dex+onehanded+twohanded+bow+crossbow) as totalstats, str, dex, onehanded, twohanded, bow, crossbow, maxmana","character_stats","characterid = "..PLAYERS[playerid].character);
+    local responses = DB_select("id, (maxmana+str+dex+onehanded+twohanded+bow+crossbow) as totalstats, lp, str, dex, onehanded, twohanded, bow, crossbow, maxmana","character_stats","characterid = "..PLAYERS[playerid].character);
     for key_, response in pairs(responses) do
         return {
             cost = math.max(1, math.ceil((tonumber(response.totalstats)-9)/10)),
@@ -80,7 +79,8 @@ function getNextTeachCost(playerid)
             twohanded = tonumber(response.twohanded),
             bow = tonumber(response.bow),
             crossbow = tonumber(response.crossbow),
-            maxmana = tonumber(response.maxmana)
+            maxmana = tonumber(response.maxmana),
+            lp = lp
         };
     end
     return {
