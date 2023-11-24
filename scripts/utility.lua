463606c43e70dc75640fa1534154505cd226f692
Rachel <shugana@gmail.com>
2023-11-24 11:21:21 +0100

    tp now also teleports right in front of target player

--- scripts/utility.lua
+++ scripts/utility.lua
@@ -106,7 +106,7 @@ function unfreeze(playerid, reason)
 end
 
 function coords_forward(angle)
-    local angle_rad = math.rad(angle);
+    local angle_rad = math.rad((angle+180)%360);
     local x = math.cos(angle_rad);
     local z = math.sin(angle_rad);
     return x, z;
