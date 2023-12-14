c56bfab0d7dda38cb901511a658ee88315654f5c
Rachel <shugana@gmail.com>
2023-12-14 04:26:41 +0100

    weather testing

--- scripts/weather.lua
+++ scripts/weather.lua
@@ -8,22 +8,32 @@ function weather(playerid, params)
     end
     
     local hour, minute = GetTime();
+    local endHour = hour;
+    local endMinute = (minute+1)%60;
+    if (minute > 58) then
+        local endHour = (hour+1)%24;
+    end
 
     if (wetter == 1) then
         debug("Wetter kein Regen");
-        SetWeather(WEATHER_RAIN, 0, hour, minute, hour, minute+1);
+        --SetWeather(WEATHER_RAIN, 0, hour, minute, hour, minute+1);
+        SetWeather(WEATHER_RAIN, 0, hour, minute, endHour, endMinute);
     elseif (wetter == 2) then
         debug("Wetter Regen ohne Blitz");
-        SetWeather(WEATHER_RAIN, 0, hour, minute, (hour+23)%24, (minute+59)%60);
+        --SetWeather(WEATHER_RAIN, 0, hour, minute+1, (hour+8)%24, (minute+59)%60);
+        SetWeather(WEATHER_RAIN, 0, endHour, endMinute, 11, 59);
     elseif (wetter == 3) then
         debug("Wetter Regen mit Blitz");
-        SetWeather(WEATHER_RAIN, 1, hour, minute, (hour+23)%24, (minute+59)%60);
+        --SetWeather(WEATHER_RAIN, 1, hour, minute+1, (hour+8)%24, (minute+59)%60);
+        SetWeather(WEATHER_RAIN, 1, endHour, endMinute, 11, 59);
     elseif (wetter == 4) then
         debug("Wetter Schnee ohne Blitz");
-        SetWeather(WEATHER_RAIN, 0, hour, minute, (hour+23)%24, (minute+59)%60);
+        --SetWeather(WEATHER_SNOW, 0, hour, minute+1, (hour+8)%24, (minute+59)%60);
+        SetWeather(WEATHER_SNOW, 0, endHour, endMinute, 11, 59);
     elseif (wetter == 5) then
         debug("Wetter Schnee mit Blitz");
-        SetWeather(WEATHER_RAIN, 1, hour, minute, (hour+23)%24, (minute+59)%60);
+        --SetWeather(WEATHER_SNOW, 1, hour, minute+1, (hour+8)%24, (minute+59)%60);
+        SetWeather(WEATHER_SNOW, 1, endHour, endMinute, 11, 59);
     else
         sendERRMessage("Unbekanntes Wetter. Versuche 1-5");
     end
